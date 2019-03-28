486222a8fdda9b2ef07528f27dd9de589c7cb855
Kacper Łukasik <sakuy124@gmail.com>
2019-03-28 22:11:00 +0100

    done

--- node.js
+++ node.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const app = express();   
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const routes = require('./routes')
+app.use(bodyParser.urlencoded({extended : false}));
 
-const myServer = http.createServer(routes);
+app.use('/admin', adminRoutes)
 
-myServer.listen(3000);
+app.use(shopRoutes)
+    
+app.use((req,res)=> {
+    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'  ))
+});
+
+const myServer = http.createServer(app);
+
+app.listen(3000);
 
 
